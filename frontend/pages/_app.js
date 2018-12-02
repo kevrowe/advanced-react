ed0f5063ad0e3040628a786a376363ad9c104bc6
Kev Rowe <kev.rowe@gmail.com>
2018-12-02 15:36:15 +0000

    Initial commit

---
+++ frontend/pages/_app.js
@@ -0,0 +1,16 @@
+import App, { Container } from "next/app";
+import Page from "../components/Page";
+
+export default class Root extends App {
+  render() {
+    const { Component } = this.props;
+
+    return (
+      <Container>
+        <Page>
+          <Component />
+        </Page>
+      </Container>
+    );
+  }
+}
