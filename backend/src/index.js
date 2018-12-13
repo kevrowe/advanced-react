8d5488edd10acca0623c77879197f99d15a02972
Kevin Rowe <kev.rowe@gmail.com>
2018-12-13 11:25:21 +0000

    Start of 14

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: ".env" });
+require("dotenv").config({ path: "variables.env" });
 
 const createServer = require("./createServer");
 const db = require("./db");
--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,13 @@
-const mutations = {};
+const mutations = {
+  async createItem(parent, args, context, info) {
+    // TODO: Check if they are logged in
+    const item = await context.db.mutation.createItem(
+      { data: { ...args } },
+      info
+    );
+
+    return item;
+  }
+};
 
 module.exports = mutations;
